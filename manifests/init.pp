9fae14451b9d7aa0f64b3b14694c0f727e43b885
Hendy Irawan <ceefour666@gmail.com>
2013-12-19 05:42:38 -0430

    vbox 4.3

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,7 +10,7 @@ class virtualbox(
     include_src => false,
   }
   package { virtualbox:
-    name => 'virtualbox-4.2',
+    name => 'virtualbox-4.3',
     ensure => present,
     require => Apt::Source['virtualbox']
   }
